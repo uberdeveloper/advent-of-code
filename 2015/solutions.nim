f5e3e040c00d8e6c23c11a2a47768b62eedf18e1
darkseid <uberdeveloper001@gmail.com>
2024-11-27 15:27:25 +0530

    second exercise updated

--- 2015/solutions.nim
+++ 2015/solutions.nim
@@ -1,4 +1,4 @@
-import strutils
+import strutils, sequtils, math
 
 proc run(f:proc, filename: string): int=
   let input = readFile(filename)
@@ -24,7 +24,53 @@ proc ex0102(input: string): int =
       return i+1
   return c
 
+proc ex0201(input: string): int =
+  let sizes = input.splitlines()
+  var c:int = 0
+  var sz:seq[int]
+  var arr:array[3, int]
+  for s in sizes:
+    try:
+      if s.len > 0:
+        sz = s.split('x').map(parseInt)
+        arr[0] = sz[0] * sz[1]
+        arr[1] = sz[1] * sz[2]
+        arr[2] = sz[0] * sz[2]
+        c += 2*sum(arr) + min(arr)
+        #echo sz, " ", sum(arr), " ", min(arr), " ", c
+    except EOFError:
+      echo "End of file reached"
+      break
+  return c
+
+
+proc ex0202(input: string): int =
+  let sizes = input.splitlines()
+  var c:int = 0
+  var sz:seq[int]
+  var arr:array[3, int]
+  for s in sizes:
+    try:
+      if s.len > 0:
+        sz = s.split('x').map(parseInt)
+        arr[0] = sz[0] + sz[1]
+        arr[1] = sz[1] + sz[2]
+        arr[2] = sz[0] + sz[2]
+        c += 2*min(arr) + (sz[0] * sz[1] * sz[2])
+        #echo sz, " ", sum(arr), " ", min(arr), " ", c
+    except EOFError:
+      echo "End of file reached"
+      break
+  return c
+    
+
 var res = run(ex0101, "data/ex01.txt")
 echo "Solution to day 1 exercise 1 = ", res
 res = run(ex0102, "data/ex01.txt")
 echo "Solution to day 1 exercise 2 = ", res
+res = run(ex0201, "data/ex02.txt")
+echo "Solution to day 2 exercise 1 = ", res
+res = run(ex0202, "data/ex02.txt")
+echo "Solution to day 2 exercise 2 = ", res
+
+
